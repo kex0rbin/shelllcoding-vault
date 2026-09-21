b0c487f3ded309615fb22e888f8e22cbe0b8b0c9
kex0rbin <175838531+kex0rbin@users.noreply.github.com>
2026-09-21 00:45:08 -0500

    Add reboot and turn off directory

---
+++ Ubuntu 18.04.5/x86-64/reboot/reboot.asm
@@ -0,0 +1,28 @@
+; ===============================================
+;  Reboot (turn off)
+;  Arch: x86-64 Linux
+;  Author: kex0rbin
+;  Syscalls: reboot (169), exit(60)
+; ===============================================
+
+section .text
+        global _start
+_start:
+
+                xor rax,rax
+                mov al,169
+                xor rdi,rdi
+                mov edi,0xfee1dead
+                xor rsi,rsi
+                mov rsi,0x28121969
+		xor rdx,rdx
+		;LINUX_REBOOT_CMD_RESTART CODE
+		mov rdx,0x01234567
+		xor r10,r10
+		syscall
+		;just in case
+		;no root
+		xor rax,rax
+		mov al,60
+		xor rdi,rdi
+		syscall
